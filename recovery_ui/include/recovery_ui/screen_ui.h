09b9baa3b7faab20711a87d692d158d799b277e9
Richard Hansen <rhansen@rhansen.org>
2020-05-12 18:08:56 -0400

    recovery: Display recovery version
    Set the title lines to the lineage version and build date and render
    them between the logo and menu.
    
    The "Lineage Recovery" and "Android Fastboot" lines are removed
    because the logo already says "Recovery" or "Fastbootd".
    
    Also: Assume the logo and back icon are non-null.
    ScreenRecoveryUI::SelectMenu already makes this assumption, and it
    avoids questions about how to handle "back" if either the logo or
    the back icon is null.
    
    Change-Id: I63bb23e34e52fdb01dd85e793552aa259f46a7de

--- recovery_ui/include/recovery_ui/screen_ui.h
+++ recovery_ui/include/recovery_ui/screen_ui.h
@@ -148,7 +148,7 @@ class TextMenu : public Menu {
   size_t MenuEnd() const;
 
   // Menu example:
-  // info:                           ColtOS Recovery
+  // info:                           LegionOS Recovery
   //                                 ....
   // help messages:                  Swipe up/down to move
   //                                 Swipe left/right to select
